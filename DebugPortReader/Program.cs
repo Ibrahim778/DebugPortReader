d1fd2d8e2e7e6de6e97f9c5d591b18b996a3f9b4
M Ibrahim <m.ibrahim0798@gmail.com>
2021-04-10 13:00:05 +0500

    Add project files.

---
+++ DebugPortReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace DebugPortReader
+{
+    class Program
+    {
+        static int port = 8080;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Setting port to: " + port);
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                StreamReader sr = new StreamReader(client.GetStream());
+                string gotString = sr.ReadLine();
+                while(gotString != null)
+                {
+                    Console.WriteLine(gotString);
+                    gotString = sr.ReadLine();
+                }
+                sr.Close();
+                sr.Dispose();
+                client.Close();
+                client.Dispose();
+            }
+            listener.Stop();
+        }
+    }
+}
